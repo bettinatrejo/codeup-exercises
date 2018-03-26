645155e1e5a29ba77c647764f0ff80de321bb27c
Bettina Trejo <bettina.trejo12@gmail.com>
2018-03-26 10:51:09 -0500

    initial commit

---
+++ src/main/java/mavenTest.java
@@ -0,0 +1,29 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+
+
+public class mavenTest {
+
+    public static void main(String[] args) {
+
+        Scanner src = new Scanner(System.in);
+        System.out.println("Enter your three favorite colors: ");
+        String input = src.nextLine();
+
+        if (StringUtils.isNumeric(input) == true) {
+                System.out.println(input  + "is a number.");
+            } else {
+                System.out.println("'" + input + "' " + "is not a number");
+            }
+
+            System.out.println("Swapped Case: " + StringUtils.swapCase(input));
+            System.out.println("Reversed: " + StringUtils.reverse(input));
+
+
+
+
+    }
+
+}
